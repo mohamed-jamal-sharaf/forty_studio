e6e5589aa4477e69a0a6607780fe72232f9dc97c
Mohamed Jamal Sharaf <mohamed.jamal.sharaf@icloud.com>
2026-02-04 08:38:52 +0200

    Auto [forty_studio]: 2026-02-04 08:38:52 - forty_studio/forty_studio_module/doctype/code_editor/

---
+++ forty_studio/forty_studio_module/doctype/code_editor/code_editor.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Capital Project and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Code Editor", {
+// 	refresh(frm) {
+
+// 	},
+// });
